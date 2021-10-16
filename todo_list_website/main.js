bc9484bf586c0c407d0529259010e1770ae80ae6
keithnelson062 <46686512+keithnelson062@users.noreply.github.com>
2021-10-15 23:43:06 -0500

    updates

---
+++ todo_list_website/main.js
@@ -0,0 +1,61 @@
+
+ const inputVal = document.getElementsByClassName('inputVal')[0];
+
+ const addTaskBtn = document.getElementsByClassName('addBtn')[0];
+ 
+
+addTaskBtn.addEventListener('click', function (){
+  
+if(inputVal.value.trim()!=0){
+       let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
+
+    }else{
+        taskList = localItems;
+    }
+    taskList.push(inputVal.value)
+    localStorage.setItem('localItem', JSON.stringify(taskList)); 
+}
+
+    showItem()
+})
+
+function showItem(){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
+
+    }else{
+        taskList = localItems;
+    }
+
+
+let html = '';
+let itemShow = document.querySelector('#myUL');
+taskList.forEach((data, index )=> {
+    
+
+    html += `
+    
+    <li >${data} <span class="close" onClick="deleteItem(${index})">X</span></li>
+    
+    
+    `
+})
+itemShow.innerHTML = html;
+}
+showItem()
+
+function deleteItem(index){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    taskList.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    showItem()
+}
+
+function clearTask(){
+    
+localStorage.clear()
+showItem()
+}
