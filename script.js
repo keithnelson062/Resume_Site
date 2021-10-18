6ddf0e003d56a97b2bfeeb8f61d83818e52b6b7f
keithnelson062 <46686512+keithnelson062@users.noreply.github.com>
2021-10-17 21:27:17 -0500

    updates

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 
 var i = 0;
+var x = 0;
+var y = 0;
+var z = 0;
 var txt = "Hi, I'm Keith Nelson";
-var speed = 75;
+var text = "I enjoy coding and watching the stock market (Thats where my interest in data science started)";
+var text1 = "I love Fullstack development and designing, testing, and programming applications";
+var text2 = "Soon to be graduate of Washington University in St. Louis";
+
+//<p id="text">I enjoy coding and watching the stock market (Thats where my interest in data science started)</p>
+
+//<p id="text1">I love Fullstack development and designing. testing, and programming applications </p>
+//<hr>
+
+//</hr><h4 id="text2">Soon to be graduate of Washington University in St. Louis</h4>
+
+var speed = 100;
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("name").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
-  }
-  
-}
 
+  }
+}
+function effecting(){
+  if (x < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(x);
+    x++;
+    setTimeout(effecting, speed);
+  }
+}
+function typeffect (){
+  if (y < text1.length) {
+    document.getElementById("text1").innerHTML += text1.charAt(y);
+    y++;
+    setTimeout(typeffect, speed);
+  }
+
+}
+function typing(){
+  if (z < text2.length) {
+    document.getElementById("text2").innerHTML += text2.charAt(z);
+    z++;
+    setTimeout(typing, speed);
+  }
+}
 
 $( function() {
   $("#accordion").accordion();
 } );
+    setTimeout(effecting, speed);
+    setTimeout(typing, speed);
+    setTimeout(typeffect, speed);
